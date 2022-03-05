41aa1167c1f8c493e8a3aaab3ad9d3669a7d17c6
Lum0o <ProGpipart@gmail.com>
2022-03-05 15:59:14 +0100

    acceuil updates

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -17,21 +17,25 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state={
-       gameArticles:[],
-       platforms:[]
+      gameArticles:[],
+      gamePlatforms:[]
     }
   }
   componentDidMount = async () => {
     const response = await fetch('http://localhost:1337/api/games?populate=*', {method: 'GET', headers: {'Accept': 'application/json', 'Content-Type':'application/json'}})
     const gameArticles = await response.json()
     this.setState({gameArticles:gameArticles})
+
+    const response2 = await fetch('http://localhost:1337/api/platforms?populate=*', {method: 'GET', headers: {'Accept': 'application/json', 'Content-Type':'application/json'}})
+    const gamePlatforms = await response2.json()
+    this.setState({gamePlatforms:gamePlatforms})
   }
 
   render() {
     return (
       <Router>
         <Routes>
-          <Route exact path='/' element={<Home platforms={this.state.platforms} gameArticles={this.state.gameArticles} />} />
+          <Route exact path='/' element={<Home gamePlatforms={this.state.gamePlatforms} gameArticles={this.state.gameArticles} />} />
           <Route exact path='/game' element={<Game gameArticles={this.state.gameArticles} />} />
           <Route exact path='/cart' element={<Cart />} />
           <Route exact path='/user' element={<User />} />
--- react-app/src/components/GameArticle.js
+++ react-app/src/components/GameArticle.js
@@ -13,29 +13,21 @@ class GameArticle extends Component {
     mouseHoverFalse = () => this.setState({mouseHover:false})
 
     render(){
+        console.log(this.props.gamePlatforms);
     return (
         <Card>
-            <Card.Img className="fluid" variant="top" src={this.props.gameArticles && "http://localhost:1337"+this.props.gameArticles.attributes.cover.data.attributes.formats.medium.url} />
+            <Card.Img className="fluid" variant="top" src={this.props.gameArticle && "http://localhost:1337"+this.props.gameArticle.attributes.cover.data.attributes.formats.medium.url} />
             <Card.Body>
-                <Card.Title className="text-center">{this.props.gameArticles && this.props.gameArticles.attributes.title}</Card.Title>
+                <Card.Title className="text-center">{this.props.gameArticle && this.props.gameArticle.attributes.title}</Card.Title>
             </Card.Body>
             <Card.Footer>
                 <Row>
-                {/*this.props.gameArticles &&
-                 this.props.gameArticles.attributes.platforms.data.id.map((id, i)=>
-                <Col key={i}>
-                    <img src={this.props.platforms.data.map((y)=>
-                     if (id == this.props.platforms.data.id[y]) {
-                        "http://localhost:1337"+this.props.platforms.data.icon.data.attributes.formats.small.url
-                    }
-                )}/>
-                </Col>)*/}
-                <Col><Button sier ="sm" variant="outline-success" onMouseOver={()=>this.mouseHoverTrue()} onMouseLeave={()=>this.mouseHoverFalse()}>{this.state.mouseHover ? "Ajouter" : "Prix"}</Button></Col>
+                {/*<img src={this.props.gameArticle && this.props.gamePlatforms.data && this.props.gamePlatforms.data.map((i)=> this.props.gamePlatforms.data[i].id == this.props.gameArticle.id && "http://localhost:1337"+this.props.gamePlatforms.data.attributes.icon.data.attributes.formats.small.url)} />*/}
+                <Col><Button sier ="sm" variant="outline-success" onMouseOver={()=>this.mouseHoverTrue()} onMouseLeave={()=>this.mouseHoverFalse()}>{this.state.mouseHover ? "Ajouter" : this.props.gameArticle && this.props.gameArticle.attributes.price+" €"}</Button></Col>
                 </Row>
             </Card.Footer>
         </Card>
     );
     }
 }
-
 export default GameArticle;
