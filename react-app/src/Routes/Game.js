a0fbf5305746fd452607ce9ccb0cb8e6c121b8d1
Lum0o <ProGpipart@gmail.com>
2022-03-06 17:54:44 +0100

    game page update

--- react-app/src/Routes/Game.js
+++ react-app/src/Routes/Game.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { CarouselItem, Container, Carousel, Row, Col, Button } from 'react-bootstrap';
+import { CarouselItem, Container, Carousel, Row, Col, Button, Stack } from 'react-bootstrap';
 import Menu from '../components/Menu';
 
 class Game extends Component {
@@ -24,10 +24,10 @@ class Game extends Component {
     return (
       <>
       <Menu/>
-      <iframe class="vid" src="https://www.youtube.com/embed/AYTjXMytBFI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <Container>
         <Row>
-          <Col xs={8} class="bg-dark">
+          <Col xs={8} className="bg-dark information">
+            {this.state.game.data && <h2 class='text-center title'>{this.state.game.data.attributes.title}</h2>}
             <Carousel>
               { this.state.game.data &&
                 this.state.game.data.attributes.media.data.map((media,i)=>
@@ -40,11 +40,48 @@ class Game extends Component {
                   </Carousel.Item>
               )}
             </Carousel>
+            <div className='genre'>
+              <h5>Genre</h5>
+
+            </div>
+            {
+              this.state.game.data &&
+              <div className="description">{this.state.game.data.attributes.description}</div>
+            }
+            {this.state.game.data &&
+            <iframe className="vid" src={this.state.game.data.attributes.vidLink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            }
           </Col>
-          <Col xs={4} class="bg-dark gameCart position-sticky">
-            {this.state.game.data && <img src={"http://localhost:1337"+this.state.game.data.attributes.cover.data.attributes.formats.small.url} alt="cover du jeu"/>}
-            {this.state.game.data && <h2 class='text-center title'>{this.state.game.data.attributes.title}</h2>}
-            <Button class="mx-auto" variant="outline-secondary">Ajouter au panier</Button>
+          <Col xs={4} className="bg-dark">
+            <div className='gameCart'>
+              {this.state.game.data && <img src={"http://localhost:1337"+this.state.game.data.attributes.cover.data.attributes.formats.small.url} alt="cover du jeu"/>}
+              {this.state.game.data && <h3 class='text-center title'>{this.state.game.data.attributes.title}</h3>}
+              <Stack gap={2} className="col-md-8 mx-auto">
+                <Button variant="secondary">ajouter au panier</Button>
+                <Button className='officialsite' variant="secondary" href={this.state.game.data && this.state.game.data.attributes.link} >site officiel</Button>
+              </Stack>
+              {this.state.game.data && <h5>{this.state.game.data.attributes.price + " €"}</h5>}
+              <div className='line'></div>
+                <Row>
+                  <Col>
+                    <h5>Développeur</h5>
+                  </Col>
+                </Row>
+                  <Col>
+                    <div className='line'></div>
+                  </Col>
+                <Row>
+                  <Col>
+                    <h5>Editeur</h5>
+                  </Col>
+                </Row>
+                  <div className='line'></div>
+                <Row>
+                  <Col>
+                    <h5>Plarformes</h5>
+                  </Col>
+                </Row>
+            </div>
           </Col>
         </Row>
       </Container>
